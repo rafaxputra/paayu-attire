03488e302073bc81db1bde2bf2785497405484e1
Rafa <rafaputra479@gmail.com>
2025-06-07 00:22:46 +0700

    update favicon ke kuning

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link rel="icon" href="{{ asset('assets/images/logos/paayu_dark_mode.png') }}" type="image/png">
+    <link rel="icon" href="{{ asset('assets/images/logos/paayu_fav.png') }}" type="image/png">
     @stack('before-styles')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
